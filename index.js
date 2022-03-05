6c2716da0ebe1c3f83df1a53af902cc937ee476d
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-03-05 14:34:07 -0500

    wrote basic employee class

---
+++ index.js
@@ -0,0 +1,10 @@
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+// Initialize a new  object
+const team = new Employee();
+
+// Set it in motion
+team.manager();
